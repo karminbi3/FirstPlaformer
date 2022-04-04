6a681b324f0c7f8bf1a3f665b4bd044eb0a51bc5
KarolinaMacionczyk <karolina1902km@gmail.com>
2022-04-04 14:01:24 +0200

    coded velocity

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -12,3 +12,11 @@ func get_direction() -> Vector2:
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input. is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
+
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		speed: Vector2,
+		direction: Vector2
+	) -> Vector2:
+	return speed * direction
